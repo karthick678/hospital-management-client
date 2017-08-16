2efeceeee4e8d2c7bb8ece93004b202e09371e72
karthick678 <karthickkumar678@gmail.com>
2017-08-16 16:13:09 +0530

    - Done Angular2 with webpack setup.

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent { }
