5a56cb6388f2f4927bd6270e2b33025fffd0eea5
karthick678 <karthickkumar678@gmail.com>
2017-12-25 22:30:59 +0530

    - Added the formbuilder

--- src/app/patient/checkup/patient.checkup.component.ts
+++ src/app/patient/checkup/patient.checkup.component.ts
@@ -5,7 +5,7 @@ import { Checkup } from '../shared/checkup.model';
 import { PatientCheckupComponentService } from './patient.checkup.service';
 import { Doctor } from './../../doctor/shared/doctor.model';
 import { DoctorDetailsService } from './../../doctor/details/doctor.details.service';
-import { FormControl } from "@angular/forms";
+import { FormGroup, FormBuilder, Validators, FormArray } from "@angular/forms";
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/startWith';
 
@@ -19,41 +19,28 @@ import 'rxjs/add/operator/startWith';
 export class PatientCheckupComponent {
     id: string;
     patientId: string;
-    checkup: Checkup;
     doctorList: any;
     whenToTakeList = [{ viewValue: '-- select --', value: '' }, { viewValue: '1-1-1', value: '1-1-1' }, { viewValue: '1-0-1', value: '1-0-1' }, { viewValue: '0-0-1', value: '0-0-1' }, { viewValue: '0-1-0', value: '0-1-0' }, { viewValue: '1-0-0', value: '1-0-0' }];
     diagnosisName: string;
-    filteredStates: Observable<any[]>;
-    stateCtrl: FormControl;
-    states: any[] = [
-        {
-            name: 'Arkansas',
-            population: '2.978M',
-        },
-        {
-            name: 'California',
-            population: '39.14M',
-        }
-    ];
+    checkupForm: FormGroup;
+    defaultPrescription = {
+        medicine: '',
+        noOfDays: 0,
+        whenToTake: '',
+        beforeMeal: false,
+    };
 
-    constructor(private router: Router, private activatedRoute: ActivatedRoute, private patientCheckupComponentService: PatientCheckupComponentService, private doctorDetailsService: DoctorDetailsService) {
-        this.stateCtrl = new FormControl();
-        this.filteredStates = this.stateCtrl.valueChanges
-            .startWith(null)
-            .map(state => state ? this.filterStates(state) : this.states.slice());
-    }
+    constructor(private router: Router, private activatedRoute: ActivatedRoute, private patientCheckupComponentService: PatientCheckupComponentService, private formBuilder: FormBuilder, private doctorDetailsService: DoctorDetailsService) {
 
-    filterStates(name: string) {
-        return this.states.filter(state =>
-            state.name.toLowerCase().indexOf(name.toLowerCase()) === 0);
     }
 
     ngOnInit() {
         this.activatedRoute.params.subscribe((params: Params) => {
+            this.buildForm();
             this.id = this.activatedRoute.snapshot.params['id'];
             this.patientId = this.activatedRoute.snapshot.params['patientId'];
-            this.checkup = this.patientCheckupComponentService.sampleCheckup();
-            this.checkup.patientId = this.patientId;
+            this.checkupForm.setValue(this.patientCheckupComponentService.sampleCheckup());
+            this.checkupForm.patchValue({ 'patientId': this.patientId });
             if (this.id !== 'new')
                 this.getCheckupDetails();
             else
@@ -81,7 +68,7 @@ export class PatientCheckupComponent {
     }
 
     onSubmit() {
-        if (this.checkup._id)
+        if (this.checkupForm.value._id)
             this.updateCheckupDetails();
         else
             this.createCheckupDetails();
@@ -89,30 +76,73 @@ export class PatientCheckupComponent {
 
     getCheckupDetails() {
         this.patientCheckupComponentService.getCheckupDetails(this.id).subscribe(checkup => {
-            this.checkup = checkup;
-            this.diagnosisName = this.checkup.diagnosis;
+            this.setCheckupForm(checkup);
         });
     }
 
+    setCheckupForm(checkup: any) {
+        this.checkupForm.patchValue({
+            doctorName: checkup.doctorName,
+            checkupDate: checkup.checkupDate,
+            symptoms: checkup.symptoms,
+            diagnosis: checkup.diagnosis,
+            extraNotes: checkup.extraNotes
+        });
+        for (let key = 0; key < checkup.prescription.length; key++) {
+            this.addPrescriptions(checkup.prescription[key]);
+        }
+        this.diagnosisName = this.checkupForm.value.diagnosis;
+    }
+
     createCheckupDetails() {
-        this.patientCheckupComponentService.createCheckupDetails(this.checkup).subscribe(checkup => {
-            this.checkup = checkup;
-            this.router.navigate(['/app/patient/checkup/' + this.patientId + '/' + this.checkup._id]);
+        this.patientCheckupComponentService.createCheckupDetails(this.checkupForm.value).subscribe(checkup => {
+            this.checkupForm.patchValue(checkup);
+            this.router.navigate(['/app/patient/checkup/' + this.patientId + '/' + this.checkupForm.value._id]);
         });
     }
 
     updateCheckupDetails() {
-        this.patientCheckupComponentService.updatePatientDetails(this.checkup).subscribe(checkup => {
-            this.checkup = checkup;
+        this.patientCheckupComponentService.updatePatientDetails(this.checkupForm.value).subscribe(checkup => {
+            this.checkupForm.patchValue(checkup);
         });
     }
 
-    deleteMedicine(index: number) {
-        this.checkup.prescription.splice(index, 1);
+    deletePrescriptions(index: number) {
+        const prescription = <FormArray>this.checkupForm.get('prescription');
+        prescription.removeAt(index);
     }
 
-    addMedicine() {
-        let prescription = this.patientCheckupComponentService.sampleCheckup().prescription[0];
-        this.checkup.prescription.push(prescription);
+    addPrescriptions(data: any) {
+        const prescription = <FormArray>this.checkupForm.get('prescription');
+        prescription.push(this.createPrescription(data));
     }
+
+    buildForm() {
+        this.checkupForm = this.formBuilder.group({
+            _id: '',
+            patientId: '',
+            doctorName: '',
+            checkupDate: new Date(),
+            symptoms: '',
+            diagnosis: '',
+            prescription: this.formBuilder.array([this.createPrescription(this.defaultPrescription)]),
+            extraNotes: ''
+        });
+    }
+
+    createPrescription(prescription: any): FormGroup {
+        return this.formBuilder.group({
+            medicine: prescription.medicine,
+            noOfDays: prescription.noOfDays,
+            whenToTake: prescription.whenToTake,
+            beforeMeal: prescription.beforeMeal
+        });
+    }
+}
+
+class Prescription {
+    medicine: string;
+    noOfDays: number;
+    whenToTake: string;
+    beforeMeal: boolean;
 }
